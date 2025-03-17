8f45fd85aac2e244cec4ec4324e8dc224b18dc9b
Kikot Vitalii <vitalii.kikot@softengi.com>
2025-03-17 10:01:33 +0100

    init

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,5 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const path = require('path');
 
 /**
  * Metro configuration
@@ -6,6 +7,15 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  *
  * @type {import('@react-native/metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+    resolver: {
+        extraNodeModules: {
+            '@': path.resolve(__dirname, 'src'),
+        },
+        sourceExts: ['tsx', 'ts', 'js', 'jsx', 'json'],
+    },
+    projectRoot: path.resolve(__dirname),
+    watchFolders: [path.resolve(__dirname, 'src')],
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
